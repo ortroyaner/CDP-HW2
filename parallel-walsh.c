89e9aa30407636db9ff76fa7d9bdaa16a6e047af
Tomnof <tom.nof@gmail.com>
2017-12-06 00:02:00 +0200

    syntax ...

--- parallel-walsh.c
+++ parallel-walsh.c
@@ -13,11 +13,11 @@ int numberOfSetBits(uint32_t i)
     return (((i + (i >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
 }
 
-int* createCol(int col, int vecSize){
+int* createCol(int colNum, int vecSize){
     int* col = (int*)malloc(sizeof(int)*vecSize);
-    if(col==null) exit(1); // it might be !col in C. not sure.
+    if(!col) exit(1);
     for(int i=0; i<vecSize; i++){
-        col[i] = (numberOfSetBits(i&col) % 2 == 0)*2 - 1;
+        col[i] = (numberOfSetBits(i&colNum) % 2 == 0)*2 - 1;
     }
     return col;
 }
@@ -29,11 +29,11 @@ void fast_parallel_walsh(int* vec, int vecSize)
 void simple_parallel_walsh(int* vec, int vecSize)
 {
     // if the vector is of size 1 or less (0), we dont need to do anything and we can just return it as is
-    if(vecSize<=1) return vec;
+    if(vecSize<=1) return;
 
     //allocate a tmp vector to save the results until we're done.
     int* tmpRes = (int*)malloc(sizeof(int)*vecSize);
-    if(tmpRes==null) exit(1);
+    if(!tmpRes) exit(1);
 
     // let's calc the value in each cell in the vector
     //in this section, we can create multiple threads to compute this part
