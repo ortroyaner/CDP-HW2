173badf6dbc50cd45cf86b36074432c7c551263a
Tomnof <tom.nof@gmail.com>
2017-12-06 00:02:41 +0200

    C project inClion - only if you want this files ..

---
+++ cmake-build-debug/CMakeFiles/DCP2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DCP2.dir/parallel-walsh.c.obj"
+  "DCP2.pdb"
+  "DCP2.exe"
+  "DCP2.exe.manifest"
+  "libDCP2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DCP2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
